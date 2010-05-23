fd546d001f4aa632d3532d37c81eb5a2e78ad473
Thomas <thedersen@gmail.com>
2010-05-23 21:52:53 +0200

    Added SynchronizationContext example

---
+++ SynchronizationContext/Application/Form.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace App
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(59, 114);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(160, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start Backgroundworker";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ SynchronizationContext/Application/Form.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            StartAsyncJob();
+        }
+
+        private void StartAsyncJob()
+        {
+            var worker = new Worker(SynchronizationContext.Current);
+
+            backgroundWorker.DoWork += (sender, e) => worker.DoWork();
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+
+    public class Worker
+    {
+        private readonly SynchronizationContext _synchronizationContext;
+
+        public Worker(SynchronizationContext synchronizationContext)
+        {
+            _synchronizationContext = synchronizationContext;
+        }
+
+        public void DoWork()
+        {
+            _synchronizationContext.Send(callback => MessageBox.Show("Hello from background worker!"), null);
+        }
+    }
+}
